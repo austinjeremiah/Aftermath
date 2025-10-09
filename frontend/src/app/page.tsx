a5f113eaccc7453aca8a80d4e0901823a364c9ed
austinjeremiah <austinjeremiah04@gmail.com>
2025-10-09 23:43:08 +0530

    Created next app and reown wallet integeration with custom push chain wallet integeration

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen relative">
+      <div className="absolute top-4 right-4">
+        <appkit-button />
+      </div>
+    </div>
+  );
+}
